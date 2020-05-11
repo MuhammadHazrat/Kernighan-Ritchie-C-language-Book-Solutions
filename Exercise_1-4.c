6ae590a749467f2c85037c7680ba41bc956272fa
Muhammad Hazrat <65144515+MuhammadHazrat@users.noreply.github.com>
2020-05-11 08:39:27 +0500

    Update Exercise_1-4.c

--- Exercise_1-4.c
+++ Exercise_1-4.c
@@ -8,9 +8,8 @@ int main()
 	float celcius;
 	printf("\n\n\t\tFarenheit to Celcius coversion\n\n");
 	printf("\t\t  Farenheit\tCelcius\n\n");
-	for (int i = 0; i <= 100; i += 20)
+	for (int farenheit = 0; farenheit <= 100; farenheit += 20)
 	{
-		farenheit = i;
 		celcius = (farenheit - 32.0) * 5.0/9.0;
 		printf("\t\t\t%3d \t %6f\n", farenheit, celcius);
 	}
